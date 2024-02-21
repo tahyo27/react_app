ec73dbcdd22055a6e5cf8f55dbe59c70439ba73a
tahyo27 <tahyo27@gmail.com>
2024-02-22 02:25:52 +0900

    ...currentArray

--- src/App.js
+++ src/App.js
@@ -2,36 +2,38 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byeFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-    return byeFn; //cleanup function 사라지면 리턴
-  }
-  useEffect(hiFn, []);
-
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  useEffect(function () {
-    console.log("hi :)");
-    return function() {
-      console.log("bye :(");
-    }
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-  </div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return; //함수 종료
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
+  <div>
+    <h1>My To Dos({toDos.length})</h1>
+      <form id="form1" onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="write your to do..." 
+        />
+        <button>Add To Do</button>
+        <hr />
+        <ul>
+          {toDos.map((item, index)=> 
+          (<li key={index}>{item}</li>))}
+        </ul>
+        
+      </form>
+  </div>
+  );
 }
 
 export default App;
